b8c1c0836af5f3cc4a7873619582a32de8ae7234
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-01-14 16:13:46 -0500

    feat: controller finalizado con metodos Jpa

--- Api/src/main/java/com/taskcrud/Api/Controller/TareaController.java
+++ Api/src/main/java/com/taskcrud/Api/Controller/TareaController.java
@@ -1,9 +1,14 @@
 package com.taskcrud.Api.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +19,44 @@ import com.taskcrud.Api.Repository.TareaRepository;
 @RestController
 @RequestMapping("/api/tareas")
 public class TareaController {
-    
     @Autowired
     private TareaRepository tareaRepository;
 
     @GetMapping
-    public Tarea crearTarea(@RequestBody Tarea tarea) { return tareaRepository.save(tarea);
-    
+    public List<Tarea> listarTarea() {
+        return tareaRepository.findAll();
+    }
+
+    @PostMapping
+    public Tarea crearTarea(@RequestBody Tarea tarea) {
+        return tareaRepository.save(tarea);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Tarea> obtenerTarea(@PathVariable Long id) {
         return tareaRepository.findById(id)
-        .map(ResponseEntity::ok)
-        .orElse(ResponseEntity.notFound().build());
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
-        
+
+    //Actualizar
+    @PutMapping("/{id}")
+    public ResponseEntity<Tarea> actualizarTarea(@PathVariable Long id, @RequestBody Tarea tareaActualizada) {
+        return tareaRepository.findById(id)
+        .map(tarea -> {
+            tarea.setTitulo(tareaActualizada.getTitulo());
+            tarea.setCompletado(tareaActualizada.isCompletado());
+            return ResponseEntity.ok(tareaRepository.save(tarea));
+        }).orElse(ResponseEntity.notFound().build());
+    }
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarTarea(@PathVariable Long id) {
+        if (tareaRepository.existsById(id)) {
+            tareaRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
